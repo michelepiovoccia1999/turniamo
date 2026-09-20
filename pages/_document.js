314c8ee9b0817e277def9cc57a78fc9d28fee3a2
Michele Piovoccia <michelepiovoccia@gmail.com>
2026-09-20 15:04:01 +0200

    aggiunta di icona app

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,12 @@ export default function Document() {
     <Html lang="it">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32.png" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#1e5fa8" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="Turniamo" />
       </Head>
       <body>
         <Main />
